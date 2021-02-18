9e3fefa213d654b781b454545118f4259bbe6f82
Cesar Henrique <cesar_silk321@hotmail.com>
2021-02-18 10:53:40 -0300

    Scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthLayoutComponent} from './auth/auth-layout.component';
+import {PagesLayoutComponent} from './pages/pages-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    component: AuthLayoutComponent,
+  },
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
+    component: PagesLayoutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
